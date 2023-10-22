54643a7c6aeb8983773949c4c9fded9296cb4884
Ben Belland <benbelland@gmail.com>
2023-10-21 20:55:36 -0700

    initial commit, it works!

---
+++ README.md
@@ -0,0 +1,2 @@
+#This is me trying out the tutorial https://lambdamod.org/post/openxr-tutorial-part-0
+
---
+++ openxr_example.cpp
@@ -0,0 +1,15 @@
+#include <vulkan/vulkan.h>
+#define XR_USE_GRAPHICS_API_VULKAN
+#include <openxr/openxr.h>
+#include <openxr/openxr_platform.h>
+
+#include <iostream>
+
+using namespace std;
+
+int main(int, char**)
+{
+	cout << "Hello world from OpenXR!" << endl;
+
+	return 0;
+}
